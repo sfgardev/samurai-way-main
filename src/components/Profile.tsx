130d06e285ef1ed8d319d5815c2978eb0042ba86
sfgardev <sf.garol@gmail.com>
2024-02-28 18:45:40 +0700

    created profile

---
+++ src/components/Profile.tsx
@@ -0,0 +1,22 @@
+const Profile = () => {
+  return (
+    <main className="content">
+      <div>
+        <img
+          src="https://cc-prod.scene7.com/is/image/CCProdAuthor/t-03-4?$pjpeg$&jpegSize=200&wid=720"
+          alt=""
+        />
+      </div>
+      <div>ava + desc</div>
+      <div>
+        my posts
+        <div>New post</div>
+      </div>
+      <div>
+        <div>Post1</div>
+        <div>Post2</div>
+      </div>
+    </main>
+  );
+};
+export default Profile;
