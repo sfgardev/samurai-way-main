3eb4737f9298bed3581b41498daa3a93e5ee6c12
sfgardev <sf.garol@gmail.com>
2024-02-28 18:45:00 +0700

    created header

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+const Header = () => {
+  return (
+    <header className="header">
+      header
+      <img
+        src="https://1000logos.net/wp-content/uploads/2017/05/Pepsi-logo.png"
+        alt=""
+      />
+    </header>
+  );
+};
+export default Header;
