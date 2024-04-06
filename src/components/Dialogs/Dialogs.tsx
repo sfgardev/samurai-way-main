0dee3ae1c29a113ecc5509097dc8f914bcba5783
sfgardev <sf.garol@gmail.com>
2024-04-06 17:17:34 +0700

    added data to dialogs + myPosts

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -25,6 +25,23 @@ const Message = (props: MessageProps) => {
 };
 
 const Dialogs = () => {
+  let dialogsData = [
+    { id: 1, name: "Dimych" },
+    { id: 2, name: "Andrey" },
+    { id: 3, name: "Sveta" },
+    { id: 4, name: "Sasha" },
+    { id: 5, name: "Viktor" },
+    { id: 6, name: "Valera" },
+  ];
+
+  let messagesData = [
+    { id: 1, message: "Hi" },
+    { id: 2, message: "How is your it-kamasutra?" },
+    { id: 3, message: "Yo!" },
+    { id: 4, message: "Yo!" },
+    { id: 5, message: "Yo!" },
+  ];
+
   return (
     <div className={s.dialogs}>
       <div className={s.dialogsItems}>
--- src/components/Profile/MyPosts/MyPosts.tsx
+++ src/components/Profile/MyPosts/MyPosts.tsx
@@ -2,6 +2,11 @@ import s from "./MyPosts.module.css";
 import Post from "./Post/Post";
 
 const MyPosts = () => {
+  let postData = [
+    { id: 1, message: "Hi, how are you?", likesCount: 12 },
+    { id: 2, message: "It's my first post", likesCount: 11 },
+  ];
+
   return (
     <div className={s.postsBlock}>
       <h3>My posts</h3>
